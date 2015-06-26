beb6aa54f74c68e9ae86fa8518fa7758a4115c0f
Dilawar Singh <dilawars@ncbs.res.in>
2015-06-26 12:02:56 +0530

    Fixed the syntax error in code.

--- SWC_Models/benchmark_all.sh
+++ SWC_Models/benchmark_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-export PYTHONPATH=/opt/moose3.0.1/python
+export PYTHONPATH=/opt/moose/python
 
 __ScriptVersion="0.1"
 
@@ -56,21 +56,21 @@ fi
 for file in $SWCFILES; do
     echo "Executing model $file"
     $PYC ./swc_loader.py -f $file -s neuron -t 1 -o nrn.png \
-        -c "hd, *dend*:*apic*, 5e-2*(1+(r*3e4))" \
-        -c "kdr, *, 100" \
-        -c "na3, *soma:*dend*:*apic*, 250" \
-        -c "nax, *axon, 1250" \
-        -c "kap, *axon:*soma*, 300" \
-        -c "kap, *dend:*apic*, 150*(1+sign(100-r*1e6)) * (1+(r*1e4))" \
-        -c "kad, *dend:*apic*, 150*(1+sign(r*1e6-100))*(1+r*1e4)"
+        -c "hd; *dend*:*apic*; 5e-2*(1+(r*3e4))" \
+        -c "kdr; *; 100" \
+        -c "na3; *soma:*dend*:*apic*; 250" \
+        -c "nax; *axon; 1250" \
+        -c "kap; *axon:*soma*; 300" \
+        -c "kap; *dend:*apic*; 150*(1+sign(100-r*1e6)) * (1+(r*1e4))" \
+        -c "kad; *dend:*apic*; 150*(1+sign(r*1e6-100))*(1+r*1e4)"
     $PYC ./swc_loader.py -f $file -s moose -t 1 \
-        -c "hd, *dend*:*apic*, 5e-2*(1+(r*3e4))" \
-        -c "kdr, *, 100" \
-        -c "na3, *soma:*dend*:*apic*, 250" \
-        -c "nax, *axon, 1250" \
-        -c "kap, *axon:*soma*, 300" \
-        -c "kap, *dend:*apic*, 150*(1+sign(100-r*1e6)) * (1+(r*1e4))" \
-        -c "kad, *dend:*apic*, 150*(1+sign(r*1e6-100))*(1+r*1e4)"
+        -c "hd; *dend*:*apic*; 5e-2*(1+(r*3e4))" \
+        -c "kdr; *; 100" \
+        -c "na3; *soma:*dend*:*apic*; 250" \
+        -c "nax; *axon; 1250" \
+        -c "kap; *axon:*soma*; 300" \
+        -c "kap; *dend:*apic*; 150*(1+sign(100-r*1e6)) * (1+(r*1e4))" \
+        -c "kad; *dend:*apic*; 150*(1+sign(r*1e6-100))*(1+r*1e4)"
 done
 
 echo "Turning topology to eps file"
