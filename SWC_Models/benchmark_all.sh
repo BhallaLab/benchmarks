b427ac61ff309a78da998569c448419d3090a72b
Dilawar Singh <dilawars@ncbs.res.in>
2015-06-26 16:17:19 +0530

    This stores data in _data dir. Create one if it does not exists.

--- SWC_Models/benchmark_all.sh
+++ SWC_Models/benchmark_all.sh
@@ -5,11 +5,14 @@ set -e
 echo "Recompiling NEURON mechanism libraries"
 nrnivmodl
 
+if [ ! -d ./_data ]; then 
+    mkdir -p _data
+fi
+
 SWCFILES=`find . -type f -name "*.swc"`
 for file in $SWCFILES; do
     ./test_moose.sh $file
     ./test_nrn.sh $file
-    break
 done
 echo "All done"
 
