295b378b84817be2be3cd5eaa8468d2e0477432d
Dilawar Singh <dilawars@ncbs.res.in>
2015-07-23 16:39:36 +0530

    Added more mechanism into chans folder.

--- moose_nrn_equivalence_testing/comparision_with_simple_HH_model/run.sh
+++ moose_nrn_equivalence_testing/comparision_with_simple_HH_model/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e
+rm -rf ./moose_results.csv ./nrn_out.dat
 export PYTHONPATH=/opt/moose/python
 python ./check_equivalence.py ../../swc_models/wu/CNG_version/ko20x-07.CNG.swc
+nrniv ./ko20x-07.CNG.swc.hoc
+./xplot.py -i ./moose_results.csv -o moose_results.png
+./xplot.py -i ./nrn_out.dat -o ./nrnout.png
--- moose_nrn_equivalence_testing/hh_model/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=/opt/moose/python
-python ./check_equivalence.py ../../swc_models/wu/CNG_version/ko20x-07.CNG.swc
