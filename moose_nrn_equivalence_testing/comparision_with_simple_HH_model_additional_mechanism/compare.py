5e516e68c52ef723f07a44d55fedcb6058701b6f
Dilawar Singh <dilawars@ncbs.res.in>
2015-07-24 20:37:17 +0530

    Not matching properly.

--- moose_nrn_equivalence_testing/comparision_with_simple_HH_model_additional_mechanism/compare.py
+++ moose_nrn_equivalence_testing/comparision_with_simple_HH_model_additional_mechanism/compare.py
@@ -16,44 +16,56 @@ __status__           = "Development"
 import sys
 import csv
 import numpy as np
+import pylab
 
 data_ = {}
 
 def get_index(query, row):
     for i, r in enumerate(row):
-        if query.lower() in r.lower():
+        r = r.split("/")[-1]
+        if query.lower() == r.lower():
             return i
-
-def get_vector(index, data):
-    res = []
-    for row in data:
-        row = row.split(",")
-        if row:
-            res.append(row[index])
-    return res
-
+    raise Exception
 
 def compare(mooseCsv, nrnCsv):
     mooseData = None
     nrnData = None
     with open(mooseCsv, "r") as f:
         mooseTxt = f.read().split("\n")
-        mooseHeader, mooseData = mooseTxt[0], mooseTxt[1:]
+        mooseHeader, mooseData = mooseTxt[0].split(","), np.genfromtxt(mooseTxt[1:],
+                delimiter=',').T
     with open(nrnCsv, "r") as f:
         nrnTxt = f.read().split("\n")
-        nrnHeader, nrnData = nrnTxt[0], nrnTxt[1:]
+        nrnHeader, nrnData = nrnTxt[0].split(','), 1e-3*np.genfromtxt(nrnTxt[1:],
+                delimiter=',').T
 
-    mooseList = mooseHeader.split(",")
-    for i, row in enumerate(nrnHeader.split(",")):
-        comptID = row.replace("table_", "")
-        mooseId = get_index(comptID, mooseList)
-        mooseVec = get_vector(mooseId, mooseData)
-        nrnVec = get_vector(i, nrnData)
-        data_[comptID] = (mooseVec, nrnVec)
+    nrnTimeVec, nrnData = nrnData[0], nrnData[1:]
+    mooseTimeVec, mooseData = mooseData[0], mooseData[1:]
+    assert np.allclose(nrnTimeVec, mooseTimeVec), mooseTimeVec - nrnTimeVec
+    for i, comptName in enumerate(nrnHeader[1:]):
+        nrnComptName = comptName.replace("table_", "")
+        mooseComptId = get_index(nrnComptName, mooseHeader)
+        print("%s - moose equivalent %s" % (i, mooseComptId))
+        pylab.plot(nrnTimeVec, nrnData[i], label="neuron")
+        pylab.plot(mooseTimeVec, mooseData[mooseComptId], label="moose")
+        print nrnComptName, mooseComptId
+        if i == 0:
+            break
+    pylab.legend(loc='best', framealpha=0.4)
+    pylab.show()
 
-    for k in data_:
-        mooseVec, nrnVec = data_[k]
-        print len(mooseVec), len(nrnVec)
+
+#   mooseList = mooseHeader.split(",")
+    #for i, row in enumerate(nrnHeader.split(",")):
+        #comptID = row.replace("table_", "")
+        #mooseId = get_index(comptID, mooseList)
+        #mooseVec = get_vector(mooseId, mooseData)
+        #nrnVec = get_vector(i, nrnData)
+        #data_[comptID] = (mooseVec, nrnVec)
+
+    #for k in data_:
+        #mooseVec, nrnVec = data_[k]
+        #print len(mooseVec), len(nrnVec)
 
 def main():
     mooseFile = sys.argv[1]
