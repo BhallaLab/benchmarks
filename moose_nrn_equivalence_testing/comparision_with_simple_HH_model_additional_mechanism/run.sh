b7efe2d60acd4b25d1f273b97b1181db7abd86f1
Dilawar Singh <dilawars@ncbs.res.in>
2015-08-05 16:06:15 +0530

    Added line in run.sh to compile channel mechanism from ../chans folder.

--- moose_nrn_equivalence_testing/comparision_with_simple_HH_model_additional_mechanism/run.sh
+++ moose_nrn_equivalence_testing/comparision_with_simple_HH_model_additional_mechanism/run.sh
@@ -3,6 +3,10 @@ set -e
 rm -rf ./moose_results.csv ./nrn_out.dat
 export PYTHONPATH=/opt/moose/python
 ./check_equivalence.py -f ../../swc_models/wu/CNG_version/ko20x-07.CNG.swc
+nrnivmodl ../chans
 nrniv ./ko20x-07.CNG.swc.hoc
 ./xplot.py -i ./moose_results.csv -o moose_results.png
 ./xplot.py -i ./nrn_out.dat -o ./nrnout.png
+# just get soma value
+echo "Plotting moose soma against nrn soma"
+
