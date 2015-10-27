2c15449c3209c0563c71a9508421b9958f1e36ca
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-27 12:36:31 +0530

    Where is kdr.

--- moose_nrn_equivalence_testing/comparision_with_simple_HH_model/run.sh
+++ moose_nrn_equivalence_testing/comparision_with_simple_HH_model/run.sh
@@ -2,10 +2,11 @@
 set -e
 set -x
 rm -rf ./moose_results.csv ./nrn_out.dat
-nrnivmodl chans
+nrnivmodl *.mod chans
 #export PYTHONPATH=/opt/moose/python
-python ./check_equivalence.py ../../swc_models/wu/CNG_version/ko20x-07.CNG.swc
-nrniv ./ko20x-07.CNG.swc.hoc
+MODELFILE=ko20x-07.CNG.swc
+python ./check_equivalence.py $MODELFILE
+nrniv $MODELFILE.hoc
 ./xplot.py -i ./moose_results.csv -o moose_results.png -t "MOOSE"
 ./xplot.py -i ./nrn_out.dat -o ./nrnout.png -t "NEURON"
 python compare.py moose_results.csv nrn_out.dat
